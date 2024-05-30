9481f93ef61b48dc6edddb6691269563aab34dfe
S30ULFR05T <kumarsuraj962536@gmail.com>
2024-05-30 12:12:58 +0530

    fixed header/navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,21 +1,115 @@
+// import React from 'react';
+// import '../stylesheet/Header.css';
+// import { Link } from 'react-router-dom';
+// import { HashLink } from 'react-router-hash-link';
+// import logo from '../images/favicon.jpg';
+// import Slogin from './Slogin';
+// import Comlogin from './Comlogin';
+// import Collogin from './Collogin';
+// import Ssignup from './Ssignup';
+// import Comsignup from './Comsignup';
+// import Colsingup from './Colsignup';
+
+
+// function Header() {
+//   return (
+//     <header className="header">
+//       <nav className="navbar">
+//         <div className="logo"><img src={logo}></img><Link to='/'>CareerConnect</Link></div>
+//         <ul className="nav-links">
+//           <li><Link to='/'>Home</Link></li>
+//           <li><HashLink to="/#features">Features</HashLink></li>
+//           <li><Link to='/about'>About Us</Link></li>
+//           <li><Link to='/Resource'>Resources</Link></li>
+//         </ul>
+
+//         <div class="dropdown">
+//         <button className="btn btn-secondary dropdown-toggle loginbutton" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+//         Sign Up
+//       </button>
+//       <ul className="dropdown-menu">
+//         <li><Link to='/Ssignup' className="dropdown-item">Student</Link></li>
+//         <li><Link to='/Comsignup' className="dropdown-item">Company</Link></li>
+//         <li><Link to='/Colsignup' className="dropdown-item">College</Link></li>
+//       </ul>
+// </div>
+
+//         <div class="dropdown">
+//         <button className="btn btn-secondary dropdown-toggle loginbutton" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+//         Login
+//       </button>
+//       <ul className="dropdown-menu">
+//         <li><Link to='/Slogin' className="dropdown-item">Student</Link></li>
+//         <li><Link to='/Comlogin' className="dropdown-item">Company</Link></li>
+//         <li><Link to='/Collogin' className="dropdown-item">College</Link></li>
+//       </ul>
+// </div>
+
+
+//       </nav>
+//     </header>
+//   );
+// }
+
+// export default Header;
+
+// import React from 'react';
+// import '../stylesheet/Header.css';
+// import { Link } from 'react-router-dom';
+// import { HashLink } from 'react-router-hash-link';
+// import logo from '../images/favicon.jpg';
+
+// function Header() {
+//   return (
+//     <header className="header">
+//       <nav className="navbar">
+//         <div className="logo"><img src={logo} alt="CareerConnect Logo" /><Link to='/'>CareerConnect</Link></div>
+//         <ul className="nav-links">
+//           <li><Link to='/'>Home</Link></li>
+//           <li><HashLink to="/#features">Features</HashLink></li>
+//           <li><Link to='/about'>About Us</Link></li>
+//           <li><Link to='/Resource'>Resources</Link></li>
+//         </ul>
+
+//         <div className="dropdown">
+//           <button className="btn btn-secondary dropdown-toggle loginbutton" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+//             Sign Up
+//           </button>
+//           <ul className="dropdown-menu">
+//             <li><Link to='/Ssignup' className="dropdown-item">Student</Link></li>
+//             <li><Link to='/Comsignup' className="dropdown-item">Company</Link></li>
+//             <li><Link to='/Colsignup' className="dropdown-item">College</Link></li>
+//           </ul>
+//         </div>
+
+//         <div className="dropdown">
+//           <button className="btn btn-secondary dropdown-toggle loginbutton" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+//             Login
+//           </button>
+//           <ul className="dropdown-menu">
+//             <li><Link to='/Slogin' className="dropdown-item">Student</Link></li>
+//             <li><Link to='/Comlogin' className="dropdown-item">Company</Link></li>
+//             <li><Link to='/Collogin' className="dropdown-item">College</Link></li>
+//           </ul>
+//         </div>
+//       </nav>
+//     </header>
+//   );
+// }
+
+// export default Header;
+
 import React from 'react';
 import '../stylesheet/Header.css';
 import { Link } from 'react-router-dom';
 import { HashLink } from 'react-router-hash-link';
 import logo from '../images/favicon.jpg';
-import Slogin from './Slogin';
-import Comlogin from './Comlogin';
-import Collogin from './Collogin';
-import Ssignup from './Ssignup';
-import Comsignup from './Comsignup';
-import Colsingup from './Colsignup';
-
 
 function Header() {
   return (
     <header className="header">
       <nav className="navbar">
-        <div className="logo"><img src={logo}></img><Link to='/'>CareerConnect</Link></div>
+        <div className="logo"><img src={logo} alt="CareerConnect Logo" /><Link to='/'>CareerConnect</Link></div>
         <ul className="nav-links">
           <li><Link to='/'>Home</Link></li>
           <li><HashLink to="/#features">Features</HashLink></li>
@@ -23,29 +117,27 @@ function Header() {
           <li><Link to='/Resource'>Resources</Link></li>
         </ul>
 
-        <div class="dropdown">
-        <button className="btn btn-secondary dropdown-toggle loginbutton" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-        Sign Up
-      </button>
-      <ul className="dropdown-menu">
-        <li><Link to='/Ssignup' className="dropdown-item">Student</Link></li>
-        <li><Link to='/Comsignup' className="dropdown-item">Company</Link></li>
-        <li><Link to='/Colsignup' className="dropdown-item">College</Link></li>
-      </ul>
-</div>
+        <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle loginbutton" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Sign Up
+          </button>
+          <ul className="dropdown-menu">
+            <li><Link to='/Ssignup' className="dropdown-item">Student</Link></li>
+            <li><Link to='/Comsignup' className="dropdown-item">Company</Link></li>
+            <li><Link to='/Colsignup' className="dropdown-item">College</Link></li>
+          </ul>
+        </div>
 
-        <div class="dropdown">
-        <button className="btn btn-secondary dropdown-toggle loginbutton" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-        Login
-      </button>
-      <ul className="dropdown-menu">
-        <li><Link to='/Slogin' className="dropdown-item">Student</Link></li>
-        <li><Link to='/Comlogin' className="dropdown-item">Company</Link></li>
-        <li><Link to='/Collogin' className="dropdown-item">College</Link></li>
-      </ul>
-</div>
-
-
+        <div className="dropdown">
+          <button className="btn btn-secondary dropdown-toggle loginbutton" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+            Login
+          </button>
+          <ul className="dropdown-menu">
+            <li><Link to='/Slogin' className="dropdown-item">Student</Link></li>
+            <li><Link to='/Comlogin' className="dropdown-item">Company</Link></li>
+            <li><Link to='/Collogin' className="dropdown-item">College</Link></li>
+          </ul>
+        </div>
       </nav>
     </header>
   );
--- src/components/Herosection.js
+++ src/components/Herosection.js
@@ -1,14 +1,72 @@
+// // import React from 'react';
+// // import '../stylesheet/HeroSection.css';
+// // import FirstImage from '../images/student1.jpg'
+// // import SecondImage from '../images/student2.jpg'
+// // import ThirdImage from '../images/student3.jpg'
+
+// // const HeroSection = () => {
+// //   return (
+// //     <section className="hero">
+// //       <div className="hero-content">
+// //       <h1>CareerConnect: Revolutionizing the Campus Hiring Process.</h1>
+// //         <p>CareerConnect is a Platform for Colleges, Universities & Students, Focused on Revolutionizing the Campus Hiring Process.</p>
+// //         <div className="cta-buttons">
+// //           <a href="#features" className="cta-button">I am a Student</a>
+// //           <a href="#features" className="cta-button">We are CRD</a>
+// //           <a href="#features" className="cta-button">We are Hiring</a>
+// //         </div>
+// //       </div>
+// //       <div className="hero-images">
+// //         <img src={FirstImage} alt="Student 1" className="hero-img" />
+// //         <img src={SecondImage} alt="Student 2" className="hero-img" />
+// //         <img src={ThirdImage} alt="Student 3" className="hero-img" />
+// //       </div>
+// //     </section>
+// //   );
+// // };
+
+// // export default HeroSection;
+
+// import React from 'react';
+// import '../stylesheet/HeroSection.css';
+// import FirstImage from '../images/student1.jpg';
+// import SecondImage from '../images/student2.jpg';
+// import ThirdImage from '../images/student3.jpg';
+
+// const HeroSection = () => {
+//   return (
+//     <section className="hero">
+//       <div className="hero-content">
+//         <h1>CareerConnect: Revolutionizing the Campus Hiring Process.</h1>
+//         <p>CareerConnect is a Platform for Colleges, Universities & Students, Focused on Revolutionizing the Campus Hiring Process.</p>
+//         <div className="cta-buttons">
+//           <a href="#features" className="cta-button">I am a Student</a>
+//           <a href="#features" className="cta-button">We are CRD</a>
+//           <a href="#features" className="cta-button">We are Hiring</a>
+//         </div>
+//       </div>
+//       <div className="hero-images">
+//         <img src={FirstImage} alt="Student 1" className="hero-img" />
+//         <img src={SecondImage} alt="Student 2" className="hero-img" />
+//         <img src={ThirdImage} alt="Student 3" className="hero-img" />
+//       </div>
+//     </section>
+//   );
+// };
+
+// export default HeroSection;
+
 import React from 'react';
 import '../stylesheet/HeroSection.css';
-import FirstImage from '../images/student1.jpg'
-import SecondImage from '../images/student2.jpg'
-import ThirdImage from '../images/student3.jpg'
+import FirstImage from '../images/student1.jpg';
+import SecondImage from '../images/student2.jpg';
+import ThirdImage from '../images/student3.jpg';
 
 const HeroSection = () => {
   return (
     <section className="hero">
       <div className="hero-content">
-      <h1>CareerConnect: Revolutionizing the Campus Hiring Process.</h1>
+        <h1>CareerConnect: Revolutionizing the Campus Hiring Process.</h1>
         <p>CareerConnect is a Platform for Colleges, Universities & Students, Focused on Revolutionizing the Campus Hiring Process.</p>
         <div className="cta-buttons">
           <a href="#features" className="cta-button">I am a Student</a>
